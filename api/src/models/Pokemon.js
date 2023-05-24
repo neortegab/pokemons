96706eee2afafac027273ca37d47f26fc1e5ab8f
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-24 07:43:28 -0500

    first commit - db added

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    speed: {
+      type: DataTypes.INTEGER
+    },
+    height: {
+      type: DataTypes.INTEGER
+    },
+    weight: {
+      type: DataTypes.INTEGER
+    }
+  });
+};
