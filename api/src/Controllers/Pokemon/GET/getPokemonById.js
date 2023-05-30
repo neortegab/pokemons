ee7c4bde007b65a9e28533dbb14db170a342cc5e
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-29 19:07:12 -0500

    Edited getPokemonsById as it wasn't looking on DB, only on axios. Now its working properly

--- api/src/Controllers/Pokemon/GET/getPokemonById.js
+++ api/src/Controllers/Pokemon/GET/getPokemonById.js
@@ -3,18 +3,19 @@ const axios = require("axios");
 
 async function getPokemonById(id){
     try {
-        
-        const apiRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const pokemonFromApi = apiRequest.data;
-        if(pokemonFromApi) return pokemonFromApi;
-        else {
-            const pokemonFromDb = await Pokemon.findOne({
-                where: {
-                    id: id
-                }
-            });
-            return pokemonFromDb;
+        let pokemonFromDb;
+        //Checks if the string parsed as an id is a number or a uuid
+        // "uuid" * 0 = NaN | 25c54bc5-1934-4f04-8fc0-4fbeeef997cb * 0 = NaN
+        // "number" * 0 = 0 | "45" * 0 = 0
+        if(id*0!=0) pokemonFromDb = await Pokemon.findByPk(id);
+        if(pokemonFromDb) return pokemonFromDb;
+        else{
+            const apiRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+            const pokemonFromApi = apiRequest.data;
+            console.log("pokemon: ", apiRequest);
+            return pokemonFromApi;
         }
+        
     } catch (error) {
         throw new Error("Error getting pokemon by id: " + error.message);
     }
