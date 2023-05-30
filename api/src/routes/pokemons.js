23676accb4746d25c1351cac26f27e60791d72ce
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-29 20:19:01 -0500

    Deleted a whitespace in pokemons router

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -13,7 +13,6 @@ router.get("/", async(req, res) => {
         }
     }
     else{
-        
         try {
             const pokemons = await controller.getPokemonByName(name);
             res.json(pokemons);
