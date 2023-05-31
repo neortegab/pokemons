8d2cd6ee52b58f33a8a572268102a49cc5b2bb65
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-30 21:29:11 -0500

    Added styling to landing page

--- client/src/components/pages/Landing.jsx
+++ client/src/components/pages/Landing.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Landing.css';
+import bulbasaur from "../../images/bulb.png";
+import charmander from "../../images/charman.png";
+import squirtle from "../../images/squirt.png";
+import logo from "../../images/pokemon_logo.png";
+
 
 export default function Landing() {
   return (
-    <div>
+    <div className='landing-container'>
+        <section className='pokemons-landing'>
+            <div id="pokemon-logo">
+                <img src={logo} alt="Pokemon logo"/>
+            </div>
+            <div className="pokemons-pics">
+                <img src={bulbasaur} alt="bulbasaur"/>
+                <img src={squirtle} id='squirtle' alt="squirtle"/>    
+                <img src={charmander} alt="charmander"/>
+            </div>
+        </section>
         <div className="login">
             Login
             <Link to="/home">
