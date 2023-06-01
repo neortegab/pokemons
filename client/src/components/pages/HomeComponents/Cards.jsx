2b205c1ec9bf9d57968a9d90faec045da4d509dc
Nelson Ortega <ne.beltran18@gmail.com>
2023-05-31 19:11:21 -0500

    Added template styles to cards and sidebar

--- client/src/components/pages/HomeComponents/Cards.jsx
+++ client/src/components/pages/HomeComponents/Cards.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import './styles/Cards.css'
 
 export default function Cards() {
   return (
-    <div>Cards</div>
+    <div className='cards-container'>Cards</div>
   )
 }
--- client/src/components/pages/HomeComponents/SideBar.jsx
+++ client/src/components/pages/HomeComponents/SideBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './styles/SideBar.css'
 
 export default function SideBar() {
   return (
-    <div>
+    <div className='sidebar-container'>
         <h2>Filters</h2>
 
         <h2>Order by</h2>
