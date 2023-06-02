0f15a025e54c440cbb9b8693fe05b4b271a4f320
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-02 17:51:32 -0500

    Cards component made

--- client/src/components/pages/HomeComponents/Cards.jsx
+++ client/src/components/pages/HomeComponents/Cards.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card';
 import './styles/Cards.css'
 
-export default function Cards() {
+export default function Cards(props) {
+  const { pokemons } = props;
+
+  function showPokemons(pokemons){
+    for(let i = 0; i < pokemons.length; i++){
+      <Card key={pokemons[i].id} pokemon={pokemons[i]}/>
+    }
+  }
   return (
-    <div className='cards-container'>Cards</div>
+    <div className='cards-container'>
+      {showPokemons(pokemons)}
+    </div>
   )
 }
