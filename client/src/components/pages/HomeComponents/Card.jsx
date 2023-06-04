947fd00b90bbdaf2828835cfa6cde9a233cc1e75
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-03 23:37:39 -0500

    Card style completed

--- client/src/components/pages/HomeComponents/Card.jsx
+++ client/src/components/pages/HomeComponents/Card.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import './styles/Card.css'
 
 export default function Card(props) {
     const { pokemon } = props;
 
-    function showTypes(){
-        pokemon.types.map(type => <p>{type}</p>);        
-    }
-
   return (
-    <div>
-        <p>{pokemon.id}</p>
-        <h3>{pokemon.name}</h3>
-        <img src={pokemon.image} alt={pokemon.name} />
-        {showTypes()}
+    <div className='pokemon-card-container'>
+      <div>
+        <div className="pokemon-card-info">
+          <h3>{pokemon.name}</h3>
+          <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+        <div className="pokemon-types-container">
+          {pokemon.types.map((type, index) => <p key={index}>{type}</p>)}
+        </div>
+      </div>
     </div>
   )
 }
