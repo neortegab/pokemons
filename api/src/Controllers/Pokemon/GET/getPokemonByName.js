b473980dde95df52db4caf1fc8fbc167eb6f3876
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-04 00:25:29 -0500

    Edited GET Pokemon by Name to show Pokemon details when called into, as previously it only showed name and url

--- api/src/Controllers/Pokemon/GET/getPokemonByName.js
+++ api/src/Controllers/Pokemon/GET/getPokemonByName.js
@@ -21,12 +21,36 @@ async function getPokemonByName(name){
                 }
             }
         });  
-        if(pokemonFromDb.length !== 0) return pokemonFromDb;
-        else{
-            const apiRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=1281`);
-            const pokemonsFromApi = apiRequest.data.results;
-            return filterPokemonByName(pokemonsFromApi, name);
+    
+        const apiRequest = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=1281`);
+        const pokemonsFromApi = apiRequest.data.results;
+        
+        let urlsFromPokemonApi = [];
+        for(let i = 0; i < pokemonsFromApi.length; i++) {
+            urlsFromPokemonApi.push(axios.get(pokemonsFromApi[i].url));
         }
+        
+        let pokemons = [];
+        await Promise.all(urlsFromPokemonApi).then(response => {
+            for(let i = 0; i < response.length; i++) {
+                const data = response[i].data;
+                const pokemon = {
+                    id: data.id,
+                    name: data.name,
+                    image: data.sprites.front_default,
+                    height: data.height,
+                    weight: data.weight,
+                    hp: data.stats[0].base_stat,
+                    attack: data.stats[1].base_stat,
+                    defense: data.stats[2].base_stat,
+                    speed: data.stats[5].base_stat,
+                    types: data.types.map(type => type.type.name),
+                };
+                pokemons.push(pokemon)
+            }
+        });
+
+        return pokemonFromDb.concat(filterPokemonByName(pokemons, name));
     } catch (error) {
         throw new Error("Error getting pokemon by id: " + error.message);
     }
