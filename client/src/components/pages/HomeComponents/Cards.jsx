3288abff6557a7f7bda94c818ab4ed73a59b5068
Nelson Ortega <ne.beltran18@gmail.com>
2023-06-04 14:55:45 -0500

    Paging working with Pokemons list, displaying pokemons correctly

--- client/src/components/pages/HomeComponents/Cards.jsx
+++ client/src/components/pages/HomeComponents/Cards.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Card from './Card';
 import './styles/Cards.css'
 
 export default function Cards(props) {
   const { pokemons } = props;
 
+  const { pageNumber } = useSelector(state => state);
+
   return (
     <div className='cards-container'>
-      {pokemons.map((pokemon, index) => {return index < 8 && <Card key={pokemon.id} pokemon={pokemon}/>})}
+      {
+        pokemons
+        .slice( 
+          (pageNumber*8)-8
+        , 
+          (pageNumber*8) 
+        )
+        .map(
+          (pokemon) =>    
+            <Card key={pokemon.id} pokemon={pokemon}/>
+        )
+      }
     </div>
   )
 }
